6db7b5f7a9e7435872720258f8cd531667551de0
Donald Plummer <dplummer@avvo.com>
2018-01-03 10:59:22 -0800

    Initial commit

---
+++ lib/plug_logger_cee.ex
@@ -0,0 +1,18 @@
+defmodule PlugLoggerCee do
+  @moduledoc """
+  Documentation for PlugLoggerCee.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugLoggerCee.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_logger_cee_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugLoggerCeeTest do
+  use ExUnit.Case
+  doctest PlugLoggerCee
+
+  test "greets the world" do
+    assert PlugLoggerCee.hello() == :world
+  end
+end
